9049ce6315c57ba1f7a19e06fc7264b8148f9227
ngerstle <ngerstle@gmail.com>
2021-07-04 14:19:57 +0200

    cleaned up

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
-# This file is pretty general, and you can adapt it in your project replacing
-# only `name` and `description` below.
+# The result of https://github.com/srid/rust-nix-template, trimmed down
 
 {
   description = "A LRU window-switcher (and more) for the sway window manager";
@@ -20,22 +19,17 @@
 
   outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
     let
-      # If you change the name here, you must also do it in Cargo.toml
       name = "swayr";
       rustChannel = "stable";
     in
     utils.lib.eachDefaultSystem
       (system:
         let
-          # Imports
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
               rust-overlay.overlay
               (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
                 rustc = self.rust-bin.${rustChannel}.latest.default;
                 cargo = self.rust-bin.${rustChannel}.latest.default;
               })
@@ -44,18 +38,13 @@
           inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
             generatedCargoNix;
 
-          # Create the cargo2nix project
           project = pkgs.callPackage
             (generatedCargoNix {
               inherit name;
               src = ./.;
             })
             {
-              # Individual crate overrides go here
-              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
               defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                # The himalaya crate itself is overriden here. Typically we
-                # configure non-Rust dependencies (see below) here.
                 ${name} = oldAttrs: {
                   inherit buildInputs nativeBuildInputs;
                 };
@@ -87,9 +76,9 @@
                 # Tools you need for development go here.
                 [
                   nixpkgs-fmt
-                  cargo-watch
                   pkgs.rust-bin.${rustChannel}.latest.rust-analysis
                   pkgs.rust-bin.${rustChannel}.latest.rls
+                  pkgs.rust-bin.${rustChannel}.latest.clippy
                 ]);
               RUST_SRC_PATH = "${pkgs.rust-bin.${rustChannel}.latest.rust-src}/lib/rustlib/src/rust/library";
             };
